c6adec85b0c4b64f1d02da9c3eb17b70c35e073b
valdezcriz144 <valdezcriz144@gmail.com>
2026-09-13 22:16:26 -0500

    Update config.py

--- config.py
+++ config.py
@@ -6,11 +6,11 @@ FILE_RAW = os.path.join(BASE_DIR, "Consolidado_Sala_Control.xlsx")
 FILE_CONSOLIDADO = os.path.join(BASE_DIR, "Consolidado_Filtrado.xlsx")
 FILE_MODELO = os.path.join(BASE_DIR, "modelo_alazan.json")
 
-LATITUD = -2.517264
-LONGITUD = -78.696058
+#LATITUD = -2.517264
+#LONGITUD = -78.696058
 
-#LATITUD = -2.5539
-#LONGITUD = -78.9286
+LATITUD = -2.5539
+LONGITUD = -78.9286
 
 POTENCIA_MAX_PERMITIDA_MW = 6.23
 CAUDAL_MAX_DISEÑO = 3.44
